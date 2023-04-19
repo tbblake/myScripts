590db526341797df522e481f5ca70167abde0fc0
Todd Blake <tbblake@gmail.com>
2023-04-19 15:19:41 -0400

    some tweaks to update the random lease generator

--- dhcpPihole/genRandomLeases.sh
+++ dhcpPihole/genRandomLeases.sh
@@ -6,11 +6,11 @@ randomMac() {
 }
 
 randomIP() {
-	printf "192.168.%d.%d" $(($RANDOM%256)) $(($RANDOM%256))
+	printf "192.168.%d.%d" $((1+$RANDOM%254)) $((1+($RANDOM%254)))
 }
 
 randomName() {
-	printf "host%02d" $(($RANDOM%41))
+	printf "host%03d" $(($RANDOM%256))
 }
 
 randomTime() {
@@ -22,22 +22,24 @@ randomTime() {
 leaseFile=$(mktemp)
 for i in $(seq 1 40)
 do
-	>&2 echo -n "$i.."
+	>&2 printf "\033[KGenerating Lease #%02d\r" $i
 	expiry=$(randomTime)
 	mac=$(randomMac)
 	ip=$(randomIP)
 	name=$(randomName)
-	while egrep -q " $mac " $leaseFile;do
+	while egrep -q " $mac " $leaseFile;do # ensure unique mac addresses
 		mac=$(randomMac)
 	done
-	while egrep -q " $ip " $leaseFile;do
+	while egrep -q " $ip " $leaseFile;do # ensure unique ip addresses
 		ip=$(randomIP)
 	done
-	while egrep -q " $name " $leaseFile;do
+	while egrep -q " $name " $leaseFile;do # ensure unique hostnames
 		name=$(randomName)
 	done
 	printf "%s %s %s %s *\n" $expiry $mac $ip $name >> $leaseFile
 done
->&2 echo
+>&2 printf "\n"
 cat $leaseFile
 rm -f $leaseFile
+
+#\033[K
