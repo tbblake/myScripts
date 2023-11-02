b807bec93029eb48762c86dd79bb33a58cbea97d
Todd Blake <tbblake@gmail.com>
2023-11-02 10:18:50 -0400

    removed scripts from repo

--- markdownConvert/downloadJS.sh
+++ markdownConvert/downloadJS.sh
@@ -5,9 +5,12 @@ cd $D
 echo Before:
 ./versions.sh
 echo
-echo cp marked.min.js marked.min.js.bk.$(date +%Y%m%d-%H%M%S) | bash -x
+echo mv marked.min.js marked.min.js.bk.$(date +%Y%m%d-%H%M%S) | bash -x
 echo
 echo curl -LO https://cdn.jsdelivr.net/npm/marked/marked.min.js | bash -x
 echo
 echo After:
 ./versions.sh
+echo
+echo Re-checking in Zabbix:
+/root/scripts/checkItem.py -n -i 39397 -i 39393
